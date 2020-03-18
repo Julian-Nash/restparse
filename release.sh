80843bf7153bc454912ee7fd96ca0db053173f02
Julian Nash <julian.nash@bramfitt-tech-labs.com>
2020-03-18 16:51:11 +0000

    Reverted to v0.0.1

--- release.sh
+++ release.sh
@@ -1,3 +1,2 @@
 python setup.py sdist
-python setup.py bdist_wheel
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="restparse",
-    version="1.2",
+    version="0.0.1",
     packages=setuptools.find_packages(),
     author="Julian Nash",
     author_email="julianjamesnash@gmail.com",
