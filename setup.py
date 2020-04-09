7010abc67cdfc8d3618d59b19e655b5346e8bf48
Julian Nash <julian.nash@bramfitt-tech-labs.com>
2020-04-09 12:04:19 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="restparse",
-    version="0.0.4",
+    version="0.0.5",
     packages=setuptools.find_packages(),
     author="Julian Nash",
     author_email="julianjamesnash@gmail.com",
